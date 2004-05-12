0e87adfdde8c3f93a06b6124eff8d0032c017514
Marius Gedminas <marius@gedmin.as>
2004-05-12 21:33:46 +0000

    Changed unit tests to use newInteraction instead of newSecurityManager.

---
+++ tests/test_fieldfactory.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Field Factory Tests
+
+$Id: test_fieldfactory.py,v 1.6 2004/04/11 18:16:27 jim Exp $
+"""
+import unittest
+
+import zope.app.schema
+
+from zope.app import zapi
+from zope.component.exceptions import ComponentLookupError
+from zope.component.interfaces import IFactory
+from zope.app.tests.placelesssetup import PlacelessSetup
+from zope.security.management import newInteraction, system_user
+from zope.schema.interfaces import IField, IText
+from zope.interface import Interface
+from zope.configuration import xmlconfig
+
+
+class ParticipationStub:
+
+    def __init__(self, principal):
+        self.principal = principal
+        self.interaction = None
+
+class IFoo(Interface): pass
+
+class TestFieldFactory(PlacelessSetup, unittest.TestCase):
+
+    def setUp(self):
+        super(TestFieldFactory, self).setUp()
+        newInteraction(ParticipationStub(system_user))
+        context = xmlconfig.file('tests/test_fieldfactory.zcml',
+                                 zope.app.schema)
+
+    def testRegisterFields(self):
+        factory = zapi.getUtility(None, IFactory,
+                                  'zope.schema._bootstrapfields.Text')
+        self.assertEquals(factory.title, "Text Field")
+        self.assertEquals(factory.description, "Text Field")
+
+    def testGetFactoriesForIField(self):
+        factories = list(zapi.getFactoriesFor(None, IField))
+        self.assertEqual(len(factories), 3)
+
+    def testGetFactoriesForIText(self):
+        factories = list(zapi.getFactoriesFor(None, IText))
+        self.assertEqual(len(factories), 2)
+
+    def testGetFactoriesUnregistered(self):
+        factories = list(zapi.getFactoriesFor(None, IFoo))
+        self.assertEqual(len(factories), 0)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite.addTest(loader.loadTestsFromTestCase(TestFieldFactory))
+    return suite
+
+
+if __name__=='__main__':
+    unittest.TextTestRunner().run(test_suite())
