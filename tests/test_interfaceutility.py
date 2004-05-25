2130b5907f08fb1dafee0bdd19d1276009ef49a9
Jim Fulton <jim@zope.com>
2004-05-25 17:27:46 +0000

    Moved the code from zope.app.traversing.__init__.py to zope.app.traversing.api to avoid a common source of circular import problems.
    I'll note:
    
    - We really need a tool for detecting circular imports. If I had such
      a tool, I would try hard to eliminate them. They are like land
      mines.
    
    - Fat __init__.py files tend to produce circular imports.
      It's useful to have thin __init__.py files that import names for
      convenience, but this needs to be done with great care.
    
    - I bet most uses of zope.app.traverse.api.traverse can go away
      now, since we don't care about context wrappers any more.
    
    - I suspect that a lot of the path-manipulation calls can go away
      as we absorb the impacts of last summer's parentgeddon.
    
    - Most of places that import zope.app.traverse.api should probably
      be converted to use zapi.

--- tests/test_interfaceutility.py
+++ tests/test_interfaceutility.py
@@ -26,7 +26,7 @@ from zope.interface import Interface, implements
 from zope.app.container.contained import Contained
 from zope.component import getService
 from zope.component.exceptions import ComponentLookupError
-from zope.app.traversing import traverse
+from zope.app.traversing.api import traverse
 from zope.app.registration.interfaces import IRegistrationStack
 from zope.app.registration.interfaces import UnregisteredStatus
 from zope.app.registration.interfaces import RegisteredStatus
