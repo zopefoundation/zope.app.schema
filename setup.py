3d08ea589f4b6c3ca545e852859d10e09287c1ed
Stephan Richter <stephan.richter@gmail.com>
2017-04-17 13:39:20 -0400

    Back to development: 3.6.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.schema',
-      version='3.6.0',
+      version='3.6.1.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Component Architecture based Vocabulary Registry',
